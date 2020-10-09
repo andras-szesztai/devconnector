8396c12ee2db50ad2d7af5c5b840af2a2f2ca741
andras-szesztai <andras@newzoo.com>
2020-10-09 13:01:44 +0200

    Delete profile, user & posts

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -124,10 +124,28 @@ router.get("/user/:user_id", async (req, res) => {
     }
     res.json(profile)
   } catch (err) {
-    console.log(err.message)
-    if(err.kind === "ObjectId"){
+    console.error(err.message)
+    if (err.kind === "ObjectId") {
       return res.status(400).json({ msg: "Profile not found" })
     }
+    res.status(500).send("Server Error")
+  }
+})
+
+// @route     DELETE api/profile
+// @desc      Delete profile, user & posts
+// @access    Private
+router.delete("/", auth, async (req, res) => {
+  try {
+    await Profile.findOneAndRemove({
+      user: req.user.id,
+    })
+    await User.findOneAndRemove({
+      _id: req.user.id,
+    })
+    res.json({msg: "User deleted"})
+  } catch (err) {
+    console.error(err.message)
     res.status(500).send("Server Error")
   }
 })
