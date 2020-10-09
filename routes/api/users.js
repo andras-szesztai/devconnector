ee8f299a089ba42865b6bd762a41655ceb4fd4a0
andras-szesztai <andras@newzoo.com>
2020-10-09 11:42:19 +0200

    login user post

--- routes/api/users.js
+++ routes/api/users.js
@@ -59,7 +59,15 @@ router.post(
         },
       }
 
-      jwt.sign(payload, config.get("jwtToken"))
+      jwt.sign(
+        payload,
+        config.get("jwtSecret"),
+        { expiresIn: 360000 },
+        (err, token) => {
+          if (err) throw err
+          res.json({ token })
+        }
+      )
     } catch (err) {
       console.error(err.message)
       res.status(500).send("Server error")
