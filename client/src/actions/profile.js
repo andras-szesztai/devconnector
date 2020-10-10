b88e44a5107f4e695588e3b49b83150b9d678cbe
andras-szesztai <andras@newzoo.com>
2020-10-10 19:35:24 +0200

    addedd getProfiles and getProfileById

--- client/src/actions/profile.js
+++ client/src/actions/profile.js
@@ -7,11 +7,37 @@ import {
   PROFILE_ERROR,
   UPDATE_PROFILE,
   DELETE_ACCOUNT,
+  GET_PROFILES,
 } from "./types"
 
 export const getCurrentProfile = () => async (dispatch) => {
   try {
     const res = await axios.get("/api/profile/me")
+    dispatch({ type: GET_PROFILE, payload: res.data })
+  } catch (err) {
+    dispatch({
+      type: PROFILE_ERROR,
+      payload: { msg: err.response.statusText, status: err.response.status },
+    })
+  }
+}
+
+export const getProfiles = () => async (dispatch) => {
+  dispatch({ type: CLEAR_PROFILE })
+  try {
+    const res = await axios.get("/api/profile")
+    dispatch({ type: GET_PROFILES, payload: res.data })
+  } catch (err) {
+    dispatch({
+      type: PROFILE_ERROR,
+      payload: { msg: err.response.statusText, status: err.response.status },
+    })
+  }
+}
+
+export const getProfileById = (userId) => async (dispatch) => {
+  try {
+    const res = await axios.get(`/api/profile/user/${userId}`)
     dispatch({ type: GET_PROFILE, payload: res.data })
   } catch (err) {
     dispatch({
