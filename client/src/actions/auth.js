4cae8ee6395fc219ea212b8c0a625969671d670d
andras-szesztai <andras@newzoo.com>
2020-10-10 14:59:05 +0200

    added loadUser action

--- client/src/actions/auth.js
+++ client/src/actions/auth.js
@@ -2,7 +2,27 @@ import axios from "axios"
 
 import { setAlert } from "./alert"
 
-import { REGISTER_SUCCESS, REGISTER_FAIL } from "./types"
+import {
+  REGISTER_SUCCESS,
+  REGISTER_FAIL,
+  USER_LOADED,
+  AUTH_ERROR,
+} from "./types"
+
+import setAuthToken from "../utils/setAuthToken"
+
+export const loadUser = () => async (dispatch) => {
+  if (localStorage.getItem("token")) {
+    setAuthToken(localStorage.getItem("token"))
+  }
+  try {
+    const res = await axios.get("/api/auth")
+    console.log("loadUser -> res", res)
+    dispatch({ type: USER_LOADED, payload: res.data })
+  } catch (err) {
+    dispatch({ type: AUTH_ERROR })
+  }
+}
 
 export const register = ({ name, email, password }) => async (dispatch) => {
   const config = {
