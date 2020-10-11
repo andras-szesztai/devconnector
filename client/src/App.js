0c74e0f38433edc49247ae7a1b533020c1f58fd1
andras-szesztai <andras@newzoo.com>
2020-10-11 09:09:55 +0200

    addded Profile

--- client/src/App.js
+++ client/src/App.js
@@ -14,6 +14,7 @@ import EditProfile from "./components/profile-form/EditProfile"
 import AddExperience from "./components/profile-form/AddExperience"
 import AddEducation from "./components/profile-form/AddEducation"
 import Profiles from "./components/profiles/Profiles"
+import Profile from "./components/profile/Profile"
 
 import store from "./store"
 import { loadUser } from "./actions/auth"
@@ -35,6 +36,7 @@ const App = () => {
             <Route exact path="/register" component={Register} />
             <Route exact path="/login" component={Login} />
             <Route exact path="/profiles" component={Profiles} />
+            <Route exact path="/profile/:id" component={Profile} />
             <PrivateRoute exact path="/dashboard" component={Dashboard} />
             <PrivateRoute
               exact
