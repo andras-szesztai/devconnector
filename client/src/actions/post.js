6abe6345e677107445639b3417b8063cda2c0e09
andras-szesztai <andras@newzoo.com>
2020-10-11 11:21:23 +0200

    CommentForm and addComment action

--- client/src/actions/post.js
+++ client/src/actions/post.js
@@ -8,6 +8,8 @@ import {
   GET_POSTS,
   POST_ERROR,
   UPDATE_LIKES,
+  ADD_COMMENT,
+  REMOVE_COMMENT,
 } from "./types"
 
 export const getPosts = () => async (dispatch) => {
@@ -81,6 +83,41 @@ export const getPost = (id) => async (dispatch) => {
   try {
     const res = await axios.get(`/api/posts/${id}`)
     dispatch({ type: GET_POST, payload: res.data })
+  } catch (err) {
+    dispatch({
+      type: POST_ERROR,
+      payload: { msg: err.response.statusText, status: err.response.status },
+    })
+  }
+}
+
+export const addComment = (postId, formData) => async (dispatch) => {
+  const config = {
+    headers: {
+      "Content-Type": "application/json",
+    },
+  }
+  try {
+    const res = await axios.post(
+      `/api/posts/comment/${postId}`,
+      formData,
+      config
+    )
+    dispatch({ type: ADD_COMMENT, payload: res.data })
+    dispatch(setAlert("Comment Added", "success"))
+  } catch (err) {
+    dispatch({
+      type: POST_ERROR,
+      payload: { msg: err.response.statusText, status: err.response.status },
+    })
+  }
+}
+
+export const deleteComment = (postId, commentId) => async (dispatch) => {
+  try {
+    const res = await axios.delete(`/api/posts/comment/${postId}/${commentId}`)
+    dispatch({ type: REMOVE_COMMENT, payload: commentId })
+    dispatch(setAlert("Comment Removed", "success"))
   } catch (err) {
     dispatch({
       type: POST_ERROR,
--- client/src/components/post/Post.js
+++ client/src/components/post/Post.js
@@ -4,6 +4,7 @@ import { Link } from "react-router-dom"
 
 import Spinner from "../layout/Spinner"
 import PostItem from "../posts/PostItem"
+import CommentForm from "./CommentForm"
 
 import { getPost } from "../../actions/post"
 
@@ -17,6 +18,7 @@ const Post = ({ getPost, post: { post, loading }, match }) => {
     <>
       <Link to="/posts" classNam="btn">Back to Posts</Link>
       <PostItem post={post} showActions={false} />
+      <CommentForm postId={post._id} />
     </>
   )
 }
