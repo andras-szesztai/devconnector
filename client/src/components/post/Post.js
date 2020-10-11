92d7c6514a271f848eac606669bff0ba0b38e1f8
andras-szesztai <andras@newzoo.com>
2020-10-11 11:32:05 +0200

    setup CommentItem

--- client/src/components/post/Post.js
+++ client/src/components/post/Post.js
@@ -5,6 +5,7 @@ import { Link } from "react-router-dom"
 import Spinner from "../layout/Spinner"
 import PostItem from "../posts/PostItem"
 import CommentForm from "./CommentForm"
+import CommentItem from "./CommentItem"
 
 import { getPost } from "../../actions/post"
 
@@ -16,9 +17,17 @@ const Post = ({ getPost, post: { post, loading }, match }) => {
     <Spinner />
   ) : (
     <>
-      <Link to="/posts" classNam="btn">Back to Posts</Link>
+      <Link to="/posts" classNam="btn">
+        Back to Posts
+      </Link>
       <PostItem post={post} showActions={false} />
       <CommentForm postId={post._id} />
+      <div className="comments">
+        {post.comments.map((comment) => (
+          <CommentItem
+           key={comment._id} comment={comment} postId={post._id} />
+        ))}
+      </div>
     </>
   )
 }
